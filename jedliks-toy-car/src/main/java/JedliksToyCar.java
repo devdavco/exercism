4119cdba2bff4b53058f8fdcd3462e926238679b
devdavco <github.ferocity308@passmail.com>
2026-07-23 20:16:16 -0500

    String.format

---
+++ jedliks-toy-car/src/main/java/JedliksToyCar.java
@@ -0,0 +1,34 @@
+public class JedliksToyCar {
+    public int batteryLife = 100;
+    public int distanceDriven = 0;
+
+    public static JedliksToyCar buy() {
+        return new JedliksToyCar();
+    }
+
+    public String distanceDisplay() {
+
+//        return "Driven " + distanceDriven + " meters";
+        return String.format("Driven %d meters",distanceDriven);
+
+    }
+
+    public String batteryDisplay() {
+
+        if(batteryLife > 0 ){
+            return "Battery at " + batteryLife + "%";
+
+        }else{
+            return "Battery empty";
+
+        }
+    }
+
+    public void drive() {
+        if(batteryLife>0){
+            distanceDriven += 20;
+            batteryLife --;
+        }
+
+    }
+}
