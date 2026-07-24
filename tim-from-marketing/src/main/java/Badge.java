6f538419db050a3af2a5b419b75727c2a2d029a2
devdavco <github.ferocity308@passmail.com>
2026-07-23 19:24:56 -0500

    null, StringBuilder, if

---
+++ tim-from-marketing/src/main/java/Badge.java
@@ -0,0 +1,19 @@
+class Badge {
+    public String print(Integer id, String name, String department) {
+
+        StringBuilder builder = new StringBuilder();
+
+
+        if(id != null){
+            builder.append("[").append(id).append("] - ");
+        }
+        builder.append(name).append(" - ");
+
+        if(department != null){
+            builder.append(department.toUpperCase());
+        }else{
+            builder.append("OWNER");
+        }
+        return builder.toString();
+    }
+}
