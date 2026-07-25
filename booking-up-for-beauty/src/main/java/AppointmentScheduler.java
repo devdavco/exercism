4e7707f596028e6f87fa85913cd0aa527fbe2a77
devdavco <github.ferocity308@passmail.com>
2026-07-25 10:23:09 -0500

    LocalDate, LocalDateTime, DateTimeFormatter.ofPattern and ternary operators

--- booking-up-for-beauty/src/main/java/AppointmentScheduler.java
+++ booking-up-for-beauty/src/main/java/AppointmentScheduler.java
@@ -1,24 +1,33 @@
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 class AppointmentScheduler {
     public LocalDateTime schedule(String appointmentDateDescription) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.schedule() method");
+        //solved 1 test
+        return LocalDateTime.parse(appointmentDateDescription, DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss"));
     }
 
     public boolean hasPassed(LocalDateTime appointmentDate) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.hasPassed() method");
+        // this exercise passed 11 test
+        return LocalDateTime.now().isAfter(appointmentDate);
     }
 
     public boolean isAfternoonAppointment(LocalDateTime appointmentDate) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.isAfternoonAppointment() method");
+        //passed 7 test
+        int hour = appointmentDate.getHour();
+
+        return hour >= 12 && hour < 18;
     }
 
     public String getDescription(LocalDateTime appointmentDate) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.getDescription() method");
+        // to add the word "at" you need to enclose to simple quotes 'at'
+        //passed 3 test
+        return "You have an appointment on " + DateTimeFormatter.ofPattern("EEEE, MMMM d, yyyy, 'at' h:mm a.").format(appointmentDate);
     }
 
     public LocalDate getAnniversaryDate() {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.getAnniversaryDate() method");
+
+        return LocalDate.of(LocalDate.now().getYear(),9,15);
     }
 }
